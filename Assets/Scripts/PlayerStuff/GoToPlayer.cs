cf71765aa7a3d30f7d243db1e9f40e03c8725071
ChaseB62 <chaseb62@educbe.ca>
2024-01-21 14:16:45 -0700

    Various changes

--- Assets/Scripts/PlayerStuff/GoToPlayer.cs
+++ Assets/Scripts/PlayerStuff/GoToPlayer.cs
@@ -11,9 +11,27 @@ public class GoToPlayer : MonoBehaviour
     public float lerpSpeed = 5f;
     public float Zoom = 10f;
 
+    public float maxFOV = 120f;
+    public float minFOV = 85f;
+
+    public Camera playerCamera;
+
     void Update()
     {
         Vector3 playerLerp = new Vector3(player.transform.position.x, player.transform.position.y, (Zoom * -1)); 
         transform.position = Vector3.Lerp(transform.position, playerLerp, Time.deltaTime * lerpSpeed);
+
+        float initialFOV = minFOV;
+
+        float fovLerpSpeed = 2f;
+
+        float normalizedMouseX = (Input.mousePosition.x / Screen.width - 0.5f) * 2f;
+        float normalizedMouseY = (Input.mousePosition.y / Screen.height - 0.5f) * 2f;
+
+        float targetFOV = Mathf.Lerp(initialFOV, maxFOV, Mathf.Max(Mathf.Abs(normalizedMouseX), Mathf.Abs(normalizedMouseY)));
+        float currentFOV = Mathf.Lerp(playerCamera.fieldOfView, targetFOV, Time.deltaTime * fovLerpSpeed);
+
+        playerCamera.fieldOfView = currentFOV;
+
     }
 }
